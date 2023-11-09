90783ce3b528b427808d894085110ac782844e87
김병준 <kbj@emgram.kr>
2023-11-09 15:17:47 +0900

    [feature/#8] 단순 사원정보 목록 조회 api 작성

---
+++ src/main/java/com/emgram/kr/dobby/controller/EmployeeController.java
@@ -0,0 +1,22 @@
+package com.emgram.kr.dobby.controller;
+
+import com.emgram.kr.dobby.dto.employee.Employee.SimpleEmployeeDTO;
+import com.emgram.kr.dobby.service.EmployeeService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
+@RequiredArgsConstructor
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    @GetMapping("/simple-info")
+    public List<SimpleEmployeeDTO> getSimpleEmployeeList() {
+        return employeeService.getSimpleEmployeeList();
+    }
+}
--- src/main/java/com/emgram/kr/dobby/dao/EmployeeDao.java
+++ src/main/java/com/emgram/kr/dobby/dao/EmployeeDao.java
@@ -1,10 +1,13 @@
 package com.emgram.kr.dobby.dao;
 
 import com.emgram.kr.dobby.dto.employee.Employee;
+import com.emgram.kr.dobby.dto.employee.Employee.SimpleEmployeeDTO;
+import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface EmployeeDao {
    Employee getEmployeeInfo(String employeeNo);
 
+   List<SimpleEmployeeDTO> findAllSimpleEmployeeList();
 }
