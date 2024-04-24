8c25fb5a6e15361b0bb600d23d4d2ebfd707f679
김준형 <brain@insightinfo.kr>
2024-04-24 21:19:52 +0900

    modal

--- src/main/java/com/emgram/kr/dobby/controller/CalendarController.java
+++ src/main/java/com/emgram/kr/dobby/controller/CalendarController.java
@@ -9,8 +9,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
+
+
+
 
 @RestController
 @RequiredArgsConstructor
@@ -21,18 +27,21 @@ public class CalendarController {
 
     @GetMapping("")
     @CrossOrigin
-    public ResponseEntity< List<Calendar> > getCalendar() {
+    public ResponseEntity< List<Calendar> > getCalendar() throws ParseException {
 
         List<Calendar> list = calendarService.getList();
         System.out.println("List<Calendar> list.size() >> "+ list.size()); //1161
 
-        List<String> listName = list.stream()
-                .map(a->a.getEventName())
-                .collect(Collectors.toList());
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = sdf.parse("2024-03-01");
 
-        for(String s : listName){
-            System.out.println("s>> "+ s);
-        }
+//        List<Calendar> listName = list.stream()
+//                .filter(a -> a.getStartDt().after(date)))
+//                .collect(Collectors.toList() );
+//
+//        for(Calendar calendar : listName){
+//            System.out.println("calendar>> "+ calendar);
+//        }
 
         return ResponseEntity.ok().body(list);
     }
